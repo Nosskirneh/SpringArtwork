191f65c08ec96cc3901e0cd2b592bf087a6aafcb
Andreas Henriksson <andreaskhenriksson@gmail.com>
2019-11-18 12:15:03 +0100

    Fixed Preferences.app being sandboxed on iOS 13

--- SettingsKeys.h
+++ SettingsKeys.h
@@ -1,6 +1,6 @@
 #define SAColor [UIColor colorWithRed:0.88 green:0.42 blue:0.04 alpha:1.0] // ~ #E26C0B
 #define SA_IDENTIFIER @"se.nosskirneh.springartwork"
-#define kPrefPath [NSString stringWithFormat:@"%@/Library/Preferences/%@.plist", NSHomeDirectory(), SA_IDENTIFIER]
+#define kPrefPath [NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", SA_IDENTIFIER]
 
 
 #define kPostNotification @"PostNotification"
